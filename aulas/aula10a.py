a9e9f2a4727964b2c07a328ba1f947b29230b2f3
EricBerlim <ericberlim@gmail.com>
2022-02-09 12:35:56 -0300

    Python - anotações

--- aulas/aula10a.py
+++
@@ -1,6 +0,0 @@
-nome = str(input('Qual é o seu nome? '))
-if nome == 'Éric':
-    print('Que nome bonito você tem!')
-else:
-    print('Seu nome é bem normal!')
-print('Bom dia, {}!'.format(nome))
--- aulas/aula11b.py
+++
@@ -1,6 +0,0 @@
-nome = 'Éric'
-cores = {'limpa': '\033[m',
-         'azul': '\033[34m',
-         'amarelo': '\033[33m',
-         'pretoebranco': '\033[7;30m'}
-print('Olá! Prazer em te conhecer, {}{}{}!'.format(cores['pretoebranco'], nome, cores['limpa']))
